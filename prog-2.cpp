a8848d87ca1516ba6583f9ed0c13e178122740f1
RyanVillenaUCR <rvill028@ucr.edu>
2019-01-30 01:54:39 -0800

    Polymorphism bugfix
    * Before, was trying to make polymorphism work with static variables
    * Changed program to now use dynamically allocated variables
    * This way, variables are initialized with the same values
    * But, functions like set_names() can now be called on whichever object**

--- prog-2.cpp
+++ prog-2.cpp
@@ -21,7 +21,10 @@ public:
 class object
 {
 public:
-    std::string& name;
+    std::string name;
+    object() : name("") {}
+
+    virtual ~object() {} //makes this class polymorphic
 };
 
 class sphere : public object
@@ -29,59 +32,72 @@ class sphere : public object
 public:
     vec center;
     double radius;
+    sphere() : center({0, 0, 0}), radius(0) {}
 };
 
 class point : public object
 {
 public:
     vec location;
+    point() : location({0, 0, 0}) {}
 };
 
-void set_names(object* objects, size_t number_objects, const std::string& base_name)
+void set_names(object** objects, size_t number_objects, const std::string& base_name)
 {
     //std::cout << "number_objects is " << number_objects << std::endl;
     for(size_t i = 0; i < number_objects; i++)
     {
         //std::cout << "i is " << i << std::endl;
         std::string temp = base_name + std::to_string(i);
-        objects[i].name = temp;
+        objects[i]->name = temp;
     }
 }
 
 int main()
 {
-    sphere* balls = new sphere[10];
-    point* points = new point[10];
+    object** balls  = new object*[10];
+    object** points = new object*[10];
 
     vec origin;
     for(size_t i = 0; i < 3; i++)
         origin[i] = 0;
     
+    for(size_t i = 0; i < 10; i++) {
+
+      balls[i] = new sphere;
+      points[i] = new point;
+    }
+
     for(size_t i = 0; i < 10; i++)
     {
-        balls[i].center = origin;
-        balls[i].radius = 0.1 * (i + 1);
-        points[i].location = origin;
-        points[i].location[0] = 0.2 * i;
+        sphere* temp_sphere = dynamic_cast<sphere*> (balls[i]);
+        assert(temp_sphere != nullptr);
+
+        temp_sphere->center = origin;
+        temp_sphere->radius = 0.1 * (i + 1);
+
+
+
+        point*  temp_point  = dynamic_cast<point*> (points[i]);
+        assert(temp_point != nullptr);
+
+        temp_point->location = origin;
+        temp_point->location[0] = 0.2 * i;
     }
 
-    for (size_t i = 0; i < 10; i++) {
-
-        std::cout << "balls[" << i << "].center: " << balls[i].center << std::endl
-                  << "balls[" << i << "].radius: " << balls[i].radius << std::endl
-                  << std::endl;
-    }
-
-    std::cout << "Right before set_names_balls" << std::endl;
     set_names(balls, 10, "ball-");
-    std::cout << "Right be3fore set_names_points" << std::endl;
     set_names(points, 10, "point-");
 
     for(size_t i = 0; i < 10; i++)
     {
-        std::cout << balls[i].name << " " << points[i].name << std::endl;
+        std::cout << balls[i]->name << " " << points[i]->name << std::endl;
     }
 
+
+    for(size_t i = 0; i < 10; i++) {
+      delete balls[i];
+      delete points[i];
+    }
     delete [] balls;
     delete [] points;
     
